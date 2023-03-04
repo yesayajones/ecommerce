3c9dc5c98c854088bb9e56fafe8b5aef13261b9f
yesayajones <yesayajones@gmail.com>
2023-03-04 06:00:16 +0200

    feat: Add index page boilerplate

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
-const Home = ({ products, bannerData }) => <div></div>;
+const Home = () => {
+	return (
+		<>
+			Hero Banner
+			<div>Best Selling Products</div>
+			<p>Speakers of many variations</p>
+			<div>{['Product 1', 'Product 2'].map((product) => product)}</div>
+			Footer
+		</>
+	);
+};
 
 export default Home;
