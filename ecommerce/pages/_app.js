dad2c7eea4760b9f570eec77227dbb0132e53050
yesayajones <yesayajones@gmail.com>
2023-03-09 07:35:04 +0200

    chore: Structure layout

--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-import '@/styles/globals.css';
-
-export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+import { Layout } from '../components';
+import '../styles/globals.css';
+function MyApp({ Component, pageProps }) {
+	return (
+		<Layout>
+			<Component {...pageProps} />
+		</Layout>
+	);
 }
+
+export default MyApp;
